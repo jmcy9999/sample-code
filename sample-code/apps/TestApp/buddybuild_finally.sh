f39a577d908660feb13d11f51ddcb207e43a8c70
jmcy9999 <janemckay@me.com>
2017-09-30 14:29:53 +0100

    init rbenv

--- sample-code/apps/TestApp/buddybuild_finally.sh
+++ sample-code/apps/TestApp/buddybuild_finally.sh
@@ -5,6 +5,8 @@ brew unlink openssl
 brew uninstall --ignore-dependencies openssl
 brew install openssl
 brew install rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 rbenv install 2.3.1
 rbenv rehash
 rbenv global 2.3.1
