e3575fdbbb6a35c8c7b1614132880b79fabc09bd
jmcy9999 <janemckay@me.com>
2017-09-30 13:45:10 +0100

    install bundler

--- sample-code/apps/TestApp/buddybuild_finally.sh
+++ sample-code/apps/TestApp/buddybuild_finally.sh
@@ -1,9 +1,11 @@
 
-bundle exec danger --fail-on-errors=true
 cd '../../examples/ruby/cucumber_ios/'
 brew install rbenv
 rbenv install 2.3.1
 rbenv rehash
 rbenv global 2.3.1
 rbenv local 2.3.1
+gem install bunder
+rbenv rehash
+bundle exec danger --fail-on-errors=true
 rake
